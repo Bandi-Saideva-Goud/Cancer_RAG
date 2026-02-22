8d9a4b1d70a7466118505ab2b87b736999dffe55
Bandi-Saideva-Goud <88177812+Bandi-Saideva-Goud@users.noreply.github.com>
2026-02-23 01:48:16 +0530

    streamlit app with open ai

--- data_ingestion/embedder.py
+++ data_ingestion/embedder.py
@@ -1,14 +1,22 @@
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 MODEL = os.getenv('EMBEDDING_MODEL')
+openai_api_key = os.getenv('OPENAI_API_KEY', None)
 
 class Embedder:
     def __init__(self):
-        self.model = HuggingFaceEmbeddings(
-                            model_name=MODEL,
-                            # model_kwargs={"device": "mps"}
-                        )
+        if MODEL == 'sentence-transformers/all-MiniLM-L6-v2':
+            self.model = HuggingFaceEmbeddings(
+                                model_name=MODEL,
+                                # model_kwargs={"device": "mps"}
+                            )
+        else:
+            self.model = OpenAIEmbeddings(
+                model = "text-embedding-3-small"
+            )
+
